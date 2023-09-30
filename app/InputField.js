5f69f0e230a5a8ba3f65914185076b03a50b7433
Hasibul <skhasibulhossain16@gmail.com>
2023-09-30 16:31:27 +0530

    push code for testing

--- app/InputField.js
+++ app/InputField.js
@@ -1,11 +1,20 @@
-'use client'
-import React ,{useRef,useState,useEffect} from 'react';
-const InputField=()=>{
-    const ref = useRef()
-    return(
+"use client";
+import React, { useRef, forwardRef, useState, useEffect } from "react";
+const InputField = forwardRef(({ getValue }, newRef) => {
+  // const ref = useRef();
+  const [inputValue, setInputValue] = useState("");
+  getValue(inputValue);
+  return (
     <div>
-      <input id="input" type="text"  ref={ref}/>
+      <input
+        id="input"
+        type="text"
+        ref={newRef}
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
+      />
     </div>
-    )
-    }
+  );
+});
 export default InputField;
--- app/page.js
+++ app/page.js
@@ -1,31 +1,39 @@
-'use client'
-import React,{useRef, useState} from 'react'
-import InputField from './InputField'
+"use client";
+import React, { useRef, useState } from "react";
+import InputField from "./InputField";
 
 function Home() {
+  const newRef = useRef();
+  const [values, setValues] = useState("");
+  const [inputValue, setInputValue] = useState("");
 
-  const newRef=useRef();
-  const [values,setValues] = useState("")
+  const getValue = (value) => {
+    setInputValue(value);
+  };
 
   const settingValue = () => {
-
-  }
+    setValues(inputValue);
+  };
 
   const focusInput = () => {
-
-  }
-
+    newRef.current.focus();
+  };
 
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/>
-    <br/>
-    <button id="settingValueButton" onClick={settingValue}>Set Value</button>
-    <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-     <textarea id="textarea" value={values}></textarea>
-
+      <InputField ref={newRef} getValue={getValue} type="text" />
+      <br />
+      <button id="settingValueButton" onClick={settingValue}>
+        Set Value
+      </button>
+      <button id="focusInputButton" onClick={focusInput}>
+        Focus the input
+      </button>
+      <br />
+      <br />
+      <textarea id="textarea" value={values}></textarea>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
